e5bb58cb3c4ec459dd6bd927cd1a5835f7d23d1e
Phero49 <pheromkuka49@gmail.com>
2023-11-30 06:48:56 +0200

    patch

--- php/userVerified.php
+++ php/userVerified.php
@@ -46,37 +46,9 @@ if (!isset($_SESSION['user_id'])) {
                     </div>
                 </div>
 
-                <div class="mb-4">
-                    <div class="d-grid gap-3 ">
-                        <button class="btn text-white " style="background-color: #f1b356
-              ;" type="button">
-                            <i class="bi bi-patch-check"></i> Verification
-                        </button>
-                        <button style="background-color: #f1b356
-              ;" class="btn text-white" type="button">
-                            <i class="bi bi-database"></i> Records
-                        </button>
-                        <button style="background-color: #f1b356
-              ;" class="btn text-white " type="button">
-                            <i class="bi bi-credit-card"></i> Billing
-                        </button>
-                        <button style="background-color: #f1b356
-              ;" class="btn text-white " type="button">
-                            <i class="bi bi-gear"></i> Service
-                        </button>
-                    </div>
-                </div>
+             
                 <div class="d-grid gap-3">
-                    <button class="btn text-white" type="button">
-                        <span>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-person" viewBox="0 0 16 16">
-                                <path
-                                    d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
-                            </svg>
-                        </span>
-                        Account
-                    </button>
+                
                     <button class="btn text-white" type="button">
                         <a href="./logout.php">
                             <span>
